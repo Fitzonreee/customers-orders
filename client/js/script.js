98e3b0f4f412607c87a815a16ebba450e9f8d090
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-02-02 15:59:21 -0800

    CustomersController and CustomerFactory for add_customer

--- client/js/script.js
+++ client/js/script.js
@@ -9,5 +9,29 @@ myApp.config(function($routeProvider){
 });
 
 // Factories
+myApp.factory('CustomersFactory', function($http){
+  var factory = {};
+
+  factory.create_customer = function(info, callback){
+    console.log(info);
+    // $http.post('/add_customer').success(function(output){
+    //   callback(output);
+    // })
+  };
+
+  return factory;
+});
 
 // Client Controllers
+myApp.controller('CustomersController', function($scope, CustomersFactory){
+  $scope.addCustomer = function(){
+    // console.log($scope.new_customer);
+    customer_repack = { name: $scope.new_customer.name,
+                        created_at: new Date()
+                      };
+    // console.log(customer_repack);
+    CustomersFactory.create_customer(customer_repack, function(data){
+      console.log("success!");
+    })
+  }
+});
